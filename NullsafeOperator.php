cbfdbfba210a130418b99fa8fbdda607d1b2a8ba
William Dobson <william.dobson@sykescottages.co.uk>
2020-12-04 17:24:59 +0000

    add basic null safe operator example

---
+++ NullsafeOperator.php
@@ -0,0 +1,27 @@
+<?php
+
+class User
+{
+    public function profile()
+    {
+        return null;
+    }
+}
+
+class Profile
+{
+    public function replyCount()
+    {
+        return 10;
+    }
+}
+
+/*
+ * this would normally throw an error as we are calling a function on null
+ * $replyCount = (new User())->profile()->replyCount();
+ * */
+
+// the nullsafe operator ?-> in the example below it will return null
+$replyCount = (new User())->profile()?->replyCount();
+
+var_dump($replyCount);
